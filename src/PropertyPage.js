586b0ba36e7d81f742cf69e3571c44dffe8b8c66
Unknown <adipaul1981@yahoo.ca>
2017-07-04 23:19:42 -0400

    update

--- src/PropertyPage.js
+++ src/PropertyPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
 
 
 
@@ -22,40 +23,30 @@ componentDidMount(){
                     return response.json() })
                         .then( (json) => {
                             this.setState({data: json});
-                            console.log("adrian");
-                            console.log(this.state.data.Address.City);
-                            console.log("adrian");
                         });
 }
 
-//componentDidUpdate() {
-//    fetch('/Property/'+this.props.params.mlsnumber)
-//                .then( (response) => {
-//                    return response.json() })
-//                        .then( (json) => {
-//                            this.setState({data: json});
-//                        });
-//};
 
 
   render() {
-      return (
-//        const styles = {this};
-            <div>
-               <div> MLS: </div>
-               <TextField
-                 id="text-field-default"
-//                                             defaultValue="toto"
-//                                               defaultValue={styles}
-                 defaultValue={this.state}
-               />
-               {console.log("WTF")};
-               {console.log(this.state)};
-               {console.log("WTF")};
-           </div>
+        if (!this.state.data) {
+            return null;
+        }
 
-      );
+        if (this.state.data) {
+            return (
+                    <Paper>
+                        <div>
+                           <div> MLS: </div>
+                           <TextField
+                             id="text-field-default"
+                             defaultValue={this.state.data.MLSNumber}
 
+                           />
+                       </div>
+                   </Paper>
+            );
+        }
   }
 
 }
