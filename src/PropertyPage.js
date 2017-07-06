e632ae104ed949754d82476a88360e2b9e021e97
Unknown <adipaul1981@yahoo.ca>
2017-07-05 23:13:28 -0400

    update

--- src/PropertyPage.js
+++ src/PropertyPage.js
@@ -23,6 +23,7 @@ componentDidMount(){
                     return response.json() })
                         .then( (json) => {
                             this.setState({data: json});
+                            console.log(json)
                         });
 }
 
@@ -35,16 +36,72 @@ componentDidMount(){
 
         if (this.state.data) {
             return (
-                    <Paper>
-                        <div>
-                           <div> MLS: </div>
-                           <TextField
-                             id="text-field-default"
-                             defaultValue={this.state.data.MLSNumber}
+                <div>
+                    <div className="row">
 
-                           />
-                       </div>
-                   </Paper>
+                        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4  ">
+                            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
+                        </div>
+
+                        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 center-xs ">
+                            <h3>Address</h3>
+                                <div>
+                                    <span>
+                                        {this.state.data.Address.PropertyNumber}
+                                    </span>
+                                    <span>
+                                        ,{this.state.data.Address.Street}
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        {this.state.data.Address.City}
+                                    </span>
+                                    <span>
+                                        ,{this.state.data.Address.Province}
+                                    </span>
+                                </div>
+                                <div>
+                                    <span>
+                                        {this.state.data.Address.Country}
+                                    </span>
+                                </div>
+                        </div>
+
+                        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 center-xs ">
+                            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
+                        </div>
+
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6  ">
+                            <Paper>
+                                <div>
+                                   <div> MLS: </div>
+                                   <TextField
+                                     id="text-field-default"
+                                     defaultValue={this.state.data.MLSNumber}
+                                   />
+                               </div>
+                            </Paper>
+                        </div>
+
+                        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+                            <Paper>
+                                <div>
+                                   <div> MLS: </div>
+                                   <TextField
+                                     id="text-field-default"
+                                     defaultValue={this.state.data.MLSNumber}
+
+                                   />
+                               </div>
+                            </Paper>
+                        </div>
+                    </div>
+
+                </div>
             );
         }
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import App from './App';
+import 'flexboxgrid/css/flexboxgrid.css';
 import './index.css';
 
 
